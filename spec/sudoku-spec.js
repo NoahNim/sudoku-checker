4a1ae9fb8358b0465f774f7f5354f5d006a1da67
Noah Medoff and Randy Weyer <randy@>
2018-07-31 11:00:30 -0700

    test horizontal numbers

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -3,7 +3,23 @@ import { sudoku } from './../src/board.js';
 describe('Sudoku', function(){
 
   it('should test if sudoku board is built', function(){
-    var testBoard = [[], [], [], [], [], [], [], [],[]];
-    expect(testBoard.size).toEqual(this.board.size);
-  })
+    var testBoard = [[], [], [], [], [], [], [], [], []];
+    var actualBoard = new sudoku();
+    expect(testBoard.length).toEqual(actualBoard.board.length);
+  });
+
+  it('should test if board does not repeat horizontally', function(){
+    var myBoard = new sudoku([
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9]
+    ]);
+    expect(myBoard.sudokuChecker()).toEqual(true);
+  });
 });
--- src/board.js
+++ src/board.js
@@ -1,6 +1,31 @@
 export class sudoku {
   constructor(board){
-    this.board = [[]];
-    // let board = [[], [], [], [], [], [], [], [],[]];
-  }
+    this.board = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+  };
+
+sudokuChecker(board){
+    for(var i = 0; i >= 9; i++){
+      for(var j = 0; j >= 9; j++){
+        for (var k = 0; i >= 9; i++) {
+          if (k != j){
+            if (board[i][k] == board[i][j]){
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return true;
+  };
 }
+// this.board[i] = Math.floor(Math.random * 10) - 1;
